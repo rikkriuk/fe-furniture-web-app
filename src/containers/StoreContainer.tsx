15061bcf6d40aa23b60eddcfd9770a64698a6cf3
ikokywu <rikiridwan949@gmail.com>
2024-12-12 20:11:25 +0700

    test: add integration testing

---
+++ src/containers/StoreContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StoreComponent from "../components/StoreComponent";
+
+const StoreContainer: React.FC = (): JSX.Element => {
+   return (
+      <section>
+         <StoreComponent />
+      </section>
+   )
+}
+
+export default StoreContainer;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderContainer from "../containers/HeaderContainer";
 import ServicesContainer from "../containers/ServicesContainer";
+import StoreContainer from "../containers/StoreContainer";
 
 const HomePage: React.FC = (): JSX.Element => {
    return (
       <main>
          <HeaderContainer />
          <ServicesContainer />
+         <StoreContainer />
       </main>
    )
 }
